fbf4469f3143c890c393574a0a1b27b42eaea80a
arghyadeep6 <arghyadeepsaha52@gmail.com>
2020-09-10 14:32:35 +0530

    NEW CONTROLLER ADDED

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getus")]
+        public string getus()
+        {
+            return "THIS IS CICD";
+        }
+    }
+}
